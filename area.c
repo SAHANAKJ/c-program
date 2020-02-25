3488076b50c06201f6c834a11ce94541c29090c1
SAHANAKJ <61144696+SAHANAKJ@users.noreply.github.com>
2020-02-25 12:20:45 +0530

    Add files via upload
    sample cprogram

---
+++ area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float main ()
+{
+float area,circumference,r;
+printf("enter the value of radius");
+scanf("%f",&r);
+area=(3.14*r*r);
+printf("area of circle=%f\n",area);
+printf("enter the value of radius");
+scanf("%f",&r);
+area=(2*3.14*r);
+printf("area of circumference of circle=%f\n",area);
+}
---
+++ peri.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int area,perimeter,l,b,h;
+printf("enter the value of length,breadth");
+scanf("%d" "%d",&l,&b);
+area=(1/2*l*b);
+printf("area of triangle=%d\n",area);
+printf("enter the value of length,breadth,height");
+scanf("%d" "%d" "%d",&l,&b,&h);
+perimeter=(3*l+b+h);
+printf("perimeter of triangle=%d\n",perimeter);
+}
