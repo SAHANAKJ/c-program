06742f36c6f42878e68517aa9a43d7012090db93
SAHANAKJ <61144696+SAHANAKJ@users.noreply.github.com>
2020-03-03 12:06:20 +0530

    Add files via upload
    cprogram

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c ;
+  printf("key in first number");
+  scanf("%d" , &a);
+  printf("key in second number");
+  scanf("%d", &b);
+  c= a+b ;
+  printf("The input value of %d & %d ax \n", a,b);
+  printf("The addition of two numbers  %d \n", c);
+}
