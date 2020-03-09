82791180903d6f7cf6000ab1619122efe8717725
SAHANAKJ <61144696+SAHANAKJ@users.noreply.github.com>
2020-03-09 16:13:34 +0530

    Add files via upload

---
+++ kjgreat3.c
@@ -0,0 +1,15 @@
+/*elseif*/
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter a,b,c values\n");
+scanf("%d%d%d",&a,&b,&c);
+if(b>a && a>c)
+printf("a\n");
+else
+if(b>a&&b>c)
+printf("b\n");
+else
+printf("c\n");
+}
